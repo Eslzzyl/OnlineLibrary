96ef517eba180f67e17ad25ef18ce6a8def5d15c
Eslzzyl <eslzzyl@163.com>
2023-12-13 16:50:19 +0800

    更新依赖

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,4 +9,18 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import { useTheme } from 'vuetify';
+
+const theme = useTheme();
+
+onMounted(() => {
+  // 检测用户操作系统颜色主题
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)')
+  theme.global.name.value = prefersDark.matches ? "dark" : "light";
+  prefersDark.addEventListener('change', (e) => {
+    theme.global.name.value = e.matches ? "dark" : "light";
+  })
+});
+
 </script>
